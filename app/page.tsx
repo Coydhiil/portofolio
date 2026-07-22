ad22118f4794b962fb630437dcaedf20b5c2003b
Coydhiil <fadhiil.fiannata@gmail.com>
2026-07-22 16:18:11 +0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <h1 className="container border mx-auto">Fadhiil</h1>
+    </div>
+  );
+}
